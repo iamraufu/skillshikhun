de4dd38021ff7093afcd1c77ce11fc04ddc49042
iamraufu <raufu.dev@gmail.com>
2022-04-21 21:23:49 +0600

    [TEST DEPLOY]

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
     return (
         <div>
             <Navbar />
-            <Hero />
+            {/* <Hero /> */}
             <FeaturedCourses />
             <SSFeatures />
             <SSModels />
