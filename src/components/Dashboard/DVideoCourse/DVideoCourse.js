bf06d3d54de992da63b88468e2154aebbbd06f42
iamraufu <raufu.dev@gmail.com>
2022-07-07 15:02:54 +0600

    [ADD] Coming Soon

--- src/components/Dashboard/DVideoCourse/DVideoCourse.js
+++ src/components/Dashboard/DVideoCourse/DVideoCourse.js
@@ -2,13 +2,14 @@ import React from 'react';
 import DNavbar from '../DNavbar/DNavbar';
 import Menu from '../Menu/Menu';
 import Sidebar from '../Sidebar/Sidebar';
+import coming_soon from '../../../images/dashboard/coming_soon.gif';
 
 const DVideoCourse = () => {
     return (
         <div className="">
             <DNavbar />
             <div className='container-fluid mt-5'>
-            <div style={{ marginTop: '5rem' }} className="row">
+                <div style={{ marginTop: '5rem' }} className="row">
 
                     {/* left sidebar */}
                     <div className="col-xl-2 col-lg-3 col-md-4 d-none d-lg-block position-sticky">
@@ -16,9 +17,12 @@ const DVideoCourse = () => {
                     </div>
 
                     {/* right container */}
-                    <div style={{ minHeight: '800px', backgroundColor: '#f3f5f9', borderRadius: '15px', marginBottom:'5rem' }} className="col-xl-9 col-lg-9 col-md-12 py-5">
+                    <div style={{ minHeight: '800px', backgroundColor: '#f3f5f9', borderRadius: '15px', marginBottom: '5rem' }} className="col-xl-9 col-lg-9 col-md-12 py-5">
 
-                        <h1 style={{ fontSize: '24px', lineHeight: '36px', color: '#343b6d', fontWeight: '700' }} className='text-center'>আপাদত কোনো ভিডিও কোর্স আপলোড করা হয়নি</h1>
+                        <h1 style={{ fontSize: '24px', lineHeight: '36px', color: '#343b6d', fontWeight: '700' }} className='text-center'>আপাতত কোনো ভিডিও কোর্স আপলোড করা হয়নি</h1>
+
+                        <img src={coming_soon} width={300} className='img-fluid mx-auto d-block' alt="coming_soon" />
+
                     </div>
                 </div>
                 <Menu />
