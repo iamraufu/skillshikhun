2d21549621e37d77f41332a384f32d34bbc53255
iamraufu <raufu.dev@gmail.com>
2022-07-25 15:26:20 +0600

    updated

--- src/components/PromoMessage/PromoMessage.js
+++ src/components/PromoMessage/PromoMessage.js
@@ -2,6 +2,7 @@ import React, { useReducer, useState } from 'react';
 import './PromoMessage.css';
 import playStore from '../../images/playStoreIcon.png';
 import { useEffect } from 'react';
+import TextSkeleton from '../Skeleton/TextSkeleton';
 
 const PromoMessage = () => {
 
@@ -25,9 +26,15 @@ const PromoMessage = () => {
         <div className="">
             <div id='promo_message_container' >
                 <div className='d-flex justify-content-center align-items-center' style={{ backgroundColor: '#653dae', height: '90px' }}>
-                    <div className='px-3 d-flex justify-content-center align-items-center'>
+                    {
+                        marketingMessage?.length ?
+                        <div className='px-3 d-flex justify-content-center align-items-center'>
                         <h1 className='promo-text text-white text-center'>{marketingMessage}</h1>
+                    </div> : 
+                    <div className="px-3 d-flex justify-content-center align-items-center">
+                        <TextSkeleton />
                     </div>
+                    }
                     <a href="https://play.google.com/store/apps/details?id=com.skillshikhun.skillshikhun" target="_blank" rel="noreferrer">
                         <div style={{ border: '1px solid lightgrey', borderRadius: '15px' }} className="p-3">
                             <img src={playStore} className='img-fluid float-right' width={120} alt="Download App" />
---
+++ src/components/Skeleton/TextSkeleton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Skeleton.css';
+import './TextSkeleton.css';
+
+const TextSkeleton = () => {
+    return (
+        <div className='text_skeleton_loading text_skeleton'></div>
+    );
+};
+
+export default TextSkeleton;
