894689bc8dd7e1f9bc6cbc5e6c2c6e7413abee0a
iamraufu <raufu.dev@gmail.com>
2022-08-04 14:41:48 +0600

    updated user

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,18 +1,25 @@
-import React, {useEffect} from 'react';
+import React
+// , {useEffect} 
+from 'react';
+// import { useParams } from 'react-router-dom';
 
 const User = () => {
     
-    const phone = localStorage.getItem('phone');
+    // const username = useParams();
+    // console.log(username);
 
-    useEffect(() => {
-        fetch(`https://skillshikhun.herokuapp.com/users/phone/${phone}`)
-            .then(res => res.json())
-            .then(data => console.log(data))
-    },[phone])
+    // useEffect(() => {
+    //     fetch(`https://skillshikhun.herokuapp.com/users/phone/${phone}`)
+    //         .then(res => res.json())
+    //         .then(data => console.log(data))
+    // },[phone])
 
     return (
-        <div style={{margin:'8rem'}}>
-            <h1>User's phone is {phone}</h1>
+        <div 
+        // style={{margin:'8rem'}}
+        >
+            <h1>EFTYKHAR RAHMAN</h1>
+            {/* <h1>User's phone is {phone}</h1> */}
         </div>
     );
 };
